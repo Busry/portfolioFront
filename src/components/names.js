b392e1662b2f5c6dde74b0902832aec64bb1f81f
busry <minitext@gmail.com>
2020-08-14 18:33:52 +0100

    try the url direct and remove proxy

--- src/components/names.js
+++ src/components/names.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Names extends Component {
-    state = {
-        users: []
-    }
+  state = {
+    loading: true,
+    users: [],
+  };
 
-    componentDidMount(){
-        fetch('/users')
-        .then(res => res.json())
-        .then(users => this.setState({
-            users
-        }));
-    }
-    render() {
-        return (
-            <div>
-               <h1>Users</h1> 
-               <ul className='hi'>
-                   {this.state.users.map( user => <li key={user.id}> {user.username} </li> )}
-               </ul>
-            </div>
-        )
-    }
+  async componentDidMount() {
+    console.log('didMount');
+    const url = 'https://portfoliosback.herokuapp.com/api/v1/users';
+    const res = await fetch(url);
+    const users = await res.json();
+    this.setState({
+      users,
+      loading: false,
+    });
+  }
+  render() {
+    const list = this.state.users.map((user) => (
+      <li key={user.id}> {user.username} </li>
+    ));
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul className="hi">{this.state.loading ? 'loading...' : list}</ul>
+      </div>
+    );
+  }
 }
 
-export default Names
+export default Names;
